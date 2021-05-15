46c34c26fbdb4dfb6a415ab3b0061e7cd0bd282d
unknown <you@example.com>
2021-05-15 18:11:37 +0700

    Tuoi

---
+++ Tuoi.js
@@ -0,0 +1,9 @@
+/*
+-b1:Đọc hiểu bài toán -> xác định input,ouput
+-b2:input (Năm sinh), output (Tuổi), thuật toán Tuổi=2021-(Năm sinh)
+-b3:vẽ lưu đồ cho bài toán
+-b4:Code theo lưu đồ
+*/
+let Namsinh=+prompt('Nhập năm sinh của bạn:');
+Tuoi=2021-Namsinh;
+alert("Tuổi của bạn:" +Tuoi)
